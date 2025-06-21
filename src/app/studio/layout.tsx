72e08ba8d9bad17cecbab3424baf84cd508caa63
Abdullah Ahmed <abdullahahmed2004@gmail.com>
2025-06-21 15:03:18 +0500

    sanity fixed

---
+++ src/app/studio/layout.tsx
@@ -0,0 +1,10 @@
+// src/app/studio/layout.tsx
+import React from "react";
+
+export default function StudioLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
